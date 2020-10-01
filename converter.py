60f85f5e15bda183ad7adf8b1891d53429c46b39
aromaljosebaby <aromaljosebaby@gmail.com>
2020-10-01 13:05:56 +0530

    edited the tkinter gui design

--- converter.py
+++ converter.py
@@ -6,15 +6,16 @@ def mcal():
 	e2.insert(0, var3)
 
 a = Tk()
+a.minsize(500,500)
 a.title("Converter")
 var1 = IntVar()
 n = "arial",14,"bold"
-Label(a,text = "Gallons", padx = 25, font=(n)).grid(row=0,sticky=W)
+Label(a,text = "Gallons", padx = 25, font=(n),fg='black', bg='SeaGreen1',  pady=10).place(x=100,y=125)
 e1 = Entry(a, width=25 , textvariable=var1)
-e1.grid(row=0,column=1)
-Label(a,text="Litres", padx=25, font=(n)).grid(row=1,sticky=W)
+e1.place(x=250,y=125)
+Label(a,text="Litres", padx=25, font=(n),fg='black', bg='SeaGreen1', pady=10).place(x=100,y=275)
 e2 = Entry(a, width=25)
-e2.grid(row=1,column=1)
-Button(a,text="calculate", font=(n), command = mcal).grid(row=2,column=1)
+e2.place(x=250,y=275)
+Button(a,text="calculate", font=(n), command = mcal, bg='SeaGreen1', padx=10, pady=10).pack(side=BOTTOM, pady=50)
 
 a.mainloop()
